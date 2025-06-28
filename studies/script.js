b074d776179e01d79e8cb5c18ba8010935c391c5
Samuel_Gomes <sg1976387@gmail.com>
2025-06-28 16:31:50 -0300

    3.2 Moving the mouse

--- studies/script.js
+++ studies/script.js
@@ -1,42 +1,19 @@
-/* --------------------#3.1 Mouse events -------------------- */
+/* --------------------#3.2 Moving the mouse -------------------- */
 
-//There are many mouse events, and just one can trigger many others mouse events.
-//All click events have the button property, which indicates which button was pressed.
+//The mouseover and mouseout have a special property called relatedTarget
+//The relatedTarget is the element that the mouse is moving to or from
 
-//The possible values of event.button are:
-// 0 - the left button
-// 1 - the middle button (usually the wheel button)
-// 2 - the right button
-// 3 - the back button (usually the side button on the left)
-// 4 - the forward button (usually the side button on the right)
+let table = document.querySelector("table");
 
-//We also can detect if the click happenend using a specific button, like alt, ctrl, or shift.
-
-let button = document.querySelector("button");
-
-button.addEventListener("click", (event) => {
-  if (event.button === 0 && event.altKey) {
-    alert("Left button clicked with Alt");
-  } else if (event.button === 0 && event.ctrlKey) {
-    alert("Left button clicked with Ctrl");
-  } else if (event.button === 0 && event.shiftKey) {
-    alert("Left button clicked with Shift");
-  } else {
-    alert("Left button clicked without any modifier keys");
-  }
+table.addEventListener("mouseover", (event) => {
+    let target = event.target;
+    if (target.tagName === "TD") {
+      target.style.backgroundColor = "lightblue";
+      event.relatedTarget.style.backgroundColor = "lightgreen";
+    }
 });
+//The related targer can be null, so be careful when using it
 
-//All mouse events have the event.clientX and event.clientY properties, which indicate the position of the mouse cursor relative to the viewport.
-//Double click event has a 'problem', it selects the text under the cursor.
-let input = document.querySelector("input");
-input.addEventListener("dblclick", (event) => {
-  event.preventDefault();
-  alert("Double click detected");
-});
-//But there are more ways of doing this
-//You can even protect your content for copy-paste, but it's not recommended.
-let h1 = document.querySelector("h1");
-h1.addEventListener("copy", (event) => {
-  event.preventDefault();
-  alert("Copying is not allowed");
-});
+//If the mouse moves from the parent to a child element, we get a mouseout event on the parent
+
+//Mouse enter and mouse leave are similar to mouseover and mouseout, but they do not bubble

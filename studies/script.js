d7a9cacb0c1cb00d75a5298e928cb2ab9f641b80
Samuel_Gomes <sg1976387@gmail.com>
2025-06-28 10:58:45 -0300

    1.10 Window sizes and scrolling

--- studies/script.js
+++ studies/script.js
@@ -1,44 +1,40 @@
-/* --------------------#1.9 Size and Scrolling -------------------- */
+/* --------------------#1.10 Window sizes and scrolling -------------------- */
 
-let lastDiv = document.getElementById('cute')
-let lastP = document.getElementById('cuteText')
+//The root document element is html element
+const root = document.documentElement;
+//If you want to get the width of the viewport, you can use the following code:
+const viewportWidth = root.clientWidth;
+//If you want to get the height of the viewport, you can use the following code:
+const viewportHeight = root.clientHeight;
+//If you want to get the width of the viewport including the scrollbar, you can use the following code:
+const viewportWidthWithScrollbar = root.offsetWidth;
+//If you want to get the height of the viewport including the scrollbar, you can use the following code:
+const viewportHeightWithScrollbar = root.offsetHeight;
 
-alert(lastP.offsetParent) //This will return the nearest ancestor that is positioned (has a position other than static)
+//To reliably obtain the full document height, we should take the maximum of these properties:
 
-//The offsetLeft/Top properties return the distance of the element relative to its offsetParent
-alert(lastP.offsetLeft + ' ' + lastP.offsetTop)
+let scrollHeight = Math.max(
+  document.body.scrollHeight, document.documentElement.scrollHeight,
+  document.body.offsetHeight, document.documentElement.offsetHeight,
+  document.body.clientHeight, document.documentElement.clientHeight
+);
 
-//There are some casses where the offsetParent is null:
-//1. The element is positioned fixed
-//2. The element is not in the document (e.g. display: none)
-//3. The element is not an HTML element (e.g. a text node)
+alert('Full document height, with scrolled out part: ' + scrollHeight);
 
-//The offsetWidth/Height properties return the width/height of the element including padding and border
-alert(lastP.offsetWidth + ' ' + lastP.offsetHeight)
 
-//The clientLeft/Top properties return the distance of the element relative to its border
-alert(lastP.clientLeft + ' ' + lastP.clientTop) //This will return 3 because the p has a border of 3px
-alert(lastDiv.clientLeft + ' ' + lastDiv.clientTop) //This will return 25 because the div has a border of 25px
+//To get the current scroll position in pixels, we can use the following code:
+alert('Current scroll position in pixels: ' + window.pageYOffset);  
+alert('Current scroll position in pixels: ' + window.pageXOffset);
 
-//The clientWidth/Height properties return the width/height of the element including padding but not border
-alert(lastDiv.clientWidth + ' ' + lastDiv.clientHeight)
+//The method scrollBy() scrolls the document by the specified number of pixels in the vertical direction.
+window.scrollBy(0, 300); // Scrolls down by 300 pixels
 
-//The scrollWidth/Height properties return the width/height of the element including the content that is not visible (the content that is scrolled)
-alert(lastDiv.scrollWidth + ' ' + lastDiv.scrollHeight)
+//The method scrollTo() scrolls the document to the specified position in the vertical direction.
+window.scrollTo(0, 300); // Scrolls to the position 300 pixels
 
-//The scrollLeft/Top properties return the distance of the element relative to its scrollable area
-alert(lastDiv.scrollLeft + ' ' + lastDiv.scrollTop)
+//The scrollIntoView() method scrolls the document so that the specified element is visible in the viewport.
+const cuteText = document.getElementById('cuteText');
+cuteText.scrollIntoView({ behavior: 'smooth' });
 
-//The scrollLeft/Top can be modified to scroll the element
-lastDiv.scrollTop = 100 //This will scroll the div 100px down
-lastDiv.scrollLeft = 100 //This will scroll the div 100px to the right
-
-//Please, don't take width/heigh of an element from the style property, because it will return the width/height without padding and border
-//Use the offsetWidth/Height, clientWidth/Height or scrollWidth/Height properties instead
-//CSS width/height depends on the box-sizing property
-//If box-sizing is set to border-box, the width/height will include padding and border
-//If box-sizing is set to content-box, the width/height will not include padding and border
-
-//CSS width, height may be auto
-//If the width/height is set to auto, the element will take the width/height of its content
-//If the width/height is set to a specific value, the element will take that value
+//if you want to make the body unscrollable, you can use the following code:
+document.body.style.overflow = 'hidden';

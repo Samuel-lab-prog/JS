ac86b5ca2b872abe9e0d8ef652b9b11ed8717d59
Samuel_Gomes <sg1976387@gmail.com>
2025-06-29 13:44:07 -0300

    3.4 Pointer events

--- studies/script.js
+++ studies/script.js
@@ -1,44 +1,22 @@
-/* --------------------#3.3 Dragging -------------------- */
+/* --------------------#3.4 Pointer events -------------------- */
 
-//The dragging and drop algorithm works like this:
-//1. mouse down on a draggable element
-//2. mouse move on the document
-//3. mouse up on the document
-const ball = document.querySelector('.ball');
-ball.onmousedown = function(event) {
+//Mouse events were replaced by pointer events in 2010.
+//Pointer events are a superset of mouse events, meaning they include all mouse events and more.
+//Pointer events are designed to handle input from various devices, including touchscreens, styluses,
+//Thet have similar properties and methods to mouse events, but they also include additional properties
+//and methods to handle touch and stylus input.
 
-  let shiftX = event.clientX - ball.getBoundingClientRect().left;
-  let shiftY = event.clientY - ball.getBoundingClientRect().top;
+//1. Pointer id
+//Each pointer event has a unique pointerId property, which is used to identify the specific pointer (e.g., finger, stylus) that is generating the event.
+//This is particularly useful for distinguishing between multiple simultaneous pointers, such as when using multi-touch gestures.
+//2. Pointer type
+//The pointerType property indicates the type of input device that generated the event, such as "
+//3.isPrimary
+//The isPrimary property indicates whether the pointer is the primary pointer for the current interaction.
 
-  ball.style.position = 'absolute';
-  ball.style.zIndex = 1000;
-  document.body.append(ball);
+//Whwn a user touches the screen with multiple fingers, each finger generates a separate pointer event. with a unique pointerId.
+//The first finger to touch the screen is considered the primary pointer, and subsequent fingers are considered secondary pointers.
 
-  moveAt(event.pageX, event.pageY);
-
-  // moves the ball at (pageX, pageY) coordinates
-  // taking initial shifts into account
-  function moveAt(pageX, pageY) {
-    ball.style.left = pageX - shiftX + 'px';
-    ball.style.top = pageY - shiftY + 'px';
-  }
-
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-  }
-
-  // move the ball on mousemove
-  document.addEventListener('mousemove', onMouseMove);
-
-  // drop the ball, remove unneeded handlers
-  ball.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    ball.onmouseup = null;
-  };
-
-};
-
-ball.ondragstart = function() {
-  return false;
-};
-
+//The pointerCancel event is fired when a pointer is no longer active, such as when a user lifts their finger off the screen or when a stylus is removed from a touchscreen.
+//The method setPointerCapture() allows an element to capture pointer events from a specific pointer, even if the pointer moves outside the element's bounds.
+//In other words, elem.setPointerCapture(pointerId) retargets all subsequent events with the given pointerId to elem.

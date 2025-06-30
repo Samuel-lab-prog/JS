1b838497a3f169f3e30514e5f8f06643526646d5
Samuel_Gomes <sg1976387@gmail.com>
2025-06-30 08:33:16 -0300

    4.2 Focus and blur

--- studies/script.js
+++ studies/script.js
@@ -1,40 +1,56 @@
-/* --------------------#4.1 Forms control -------------------- */
+/* --------------------#4.2 Focus and blur -------------------- */
 
-// Document forms are members of the special collection document.forms.
-// The collection is an array-like object, which means that it has a length property and can be accessed by index.
-// The collection is live, meaning that it is automatically updated when the document changes.
+// Focus is the ability to direct the user's attention to a specific element on the page.
+// This is often used for form inputs, buttons, and other interactive elements.
+// The focus can be set programmatically using JavaScript, and it can also be managed through keyboard navigation.
+// Losing the focus (blur), means that the data in the input field is no longer being edited.
 
-let firstForm = document.forms[0]; // Get the first form in the document
-let secondForm = document.forms[1]; // Get the second form in the document
-// You can also access forms by their name or id attribute
-let namedForm = document.forms['firstForm']; // Get the form with name 'firstForm'
-let idForm = document.forms['secondForm']; // Get the form with id 'secondForm
+// Focus and blur are events. Here's a quick example of how to use them:
 
-//Inside the collection, we may have many elements with the same name, in this case, if we acess the
-//Element by name, we will get a collection of elements with that name
+let myEmail = document.getElementById('emailInput');
+let error = document.getElementById('emailError');
+myEmail.onblur = function() {
+  if(!myEmail.value.includes('@')) 
+    error.innerHTML = 'Please enter a valid email address.';
+  else
+    myEmail.style.backgroundColor = error.innerHTML = '';
+}
+myEmail.onfocus = function() {
+    error.innerHTML = '';
+}
 
-let radioButtons = firstForm.option; // Get all radio buttons with name 'option' in the first form
-//Since this is a collection, we can iterate over it
-for (let i = 0; i < radioButtons.length; i++) 
-    alert(radioButtons[i].value); 
+// The methods focus() and blur() can be used to set or remove focus from an element.
+// Let's make the user not be able to exist the input field until they enter a valid email address:
+let secondEmail = document.getElementById('secondEmailInput');
+secondEmail.onblur = function() {
+  if(!secondEmail.value.includes('@')) {
+    error.innerHTML = 'Please enter a valid email address.';
+    secondEmail.focus(); // Set focus back to the input field
+  } else {
+    error.innerHTML = '';
+  }
+}
 
-//The acess to the elements does not depends on the tag structure, so we can access the elements by their name or id
+// Some elements are not focusable by default, such as <div> or <span>.
+// To make them focusable, you can add the tabindex attribute:
 
-//You can get the form that an element belongs to by using the form property of the element
-let inputElement = document.querySelector('input'); 
-alert(inputElement.form.id); // Get the form that the input element belongs to
+let myList = document.getElementById('list');
+let listItems = myList.getElementsByTagName('li');
+listItems[0].tabIndex = 1; // Make the first list item focusable
+listItems[1].tabIndex = 2; // Make the second list item focusable
+listItems[2].tabIndex = 0; // Skips the third list item
+// This means that the third list item will not be focusable, but the fourth will be
+listItems[3].tabIndex = 4; // Make the fourth list item focusable
+listItems[4].tabIndex = -1; // Make the fifth list item not focusable
 
-//To acess the content of an input element, you can use the value property
-let inputValue = inputElement.value; // Get the value of the input element
-//If the input element is a checkbox or radio button, you can use the checked property
-let isChecked = inputElement.checked; // Get the checked state of the input element
+// Blur and focus events do not bubble, meaning they do not propagate up the DOM tree.
+// This means that if you have an event listener on a parent element, it will not be triggered when a child element receives focus or loses focus.
+// However, you can use the focusin and focusout events, which do bubble:
 
-//The select element has 3 special properties: options, selectedIndex, and value
-// 1.1 Options: a collection of all the options in the select element
-// 1.2 selectedValue: the value of the selected option
-// 1.3 selectedIndex: the index of the selected option
+myList.addEventListener('focusin', function(event) {
+    event.target.style.backgroundColor = 'lightblue'; // Change background color of focused element
+});
 
-//There's a short syntax to create a new option element and add it to the select element
-option = new Option('Option 1', 'value1', false, false); // Create a new option element;
-
-//Other input elements may have special properties such as text, but the most common ones are value and checked
+myList.addEventListener('focusout', function(event) {
+    event.target.style.backgroundColor = ''; // Reset background color when focus is lost
+});

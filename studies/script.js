0158398310a8caca74f53fdb7e6dbcf5caa5906a
Samuel_Gomes <sg1976387@gmail.com>
2025-06-30 09:27:58 -0300

    5.2 defer and async

--- studies/script.js
+++ studies/script.js
@@ -1,40 +1,12 @@
-/* --------------------#5.1 Page events-------------------- */
+/* --------------------#5.2 defer and async-------------------- */
 
-//The lifecycle of an HTML page has three important events:
-//DOMContentLoaded – the browser fully loaded HTML, and the DOM tree is built, but external resources like pictures <img> and stylesheets may not yet have loaded.
-//load – not only HTML is loaded, but also all the external resources: images, styles etc.
-//beforeunload/unload – the user is leaving the page.
-//All these are events that we can listen to and react to.
-document.addEventListener("DOMContentLoaded", function() {
-    alert("DOM fully loaded and parsed");
-});
-//One important thing to note is that the DOMContentLoaded has to read all scripts before it can fire
-//so if you have a script that is loaded before the DOMContentLoaded event, it will not be able to access the DOM elements that are not yet loaded.
-//To avoid this, you can either place your script at the end of the body or use
+//The defer attribute is a boolean attribute that can be added to the <script> tag.
+//When present, it indicates that the script should be executed after the document has been parsed.
+//This means that the script will not block the rendering of the page, and the browser can continue to load and display the content while the script is being fetched and executed.
 
-//The load event is fired when the whole page is loaded, including all dependent resources such as stylesheets and images.
-window.addEventListener("load", function() {
-    alert("Page fully loaded");
-});
-//The unload event is fired when the user is leaving the page, for example, when they close the tab or navigate to another page.
-window.addEventListener("unload", function(event) {
-    alert("You are leaving the page");
-});
+//The async attribute is also a boolean attribute that can be added to the <script> tag.
+//When present, it indicates that the script should be executed asynchronously as soon as it is available.
+//This means that the script will be fetched and executed in parallel with the rest of the page, without blocking the rendering of the content.
 
-window.onbeforeunload = function(event) {
-    return "Are you sure you want to leave this page? Stay with us!";
-}
-//The readyState property of the document object can be used to check the current state of the page.
-//It can have three values:
-//loading – the document is still loading
-//interactive – the document has finished loading and the DOM is ready, but external resources like images may still be loading
-//complete – the document and all external resources are fully loaded
-document.onreadystatechange = function() {
-    if (document.readyState === "loading") {
-        alert("Document is loading");
-    } else if (document.readyState === "interactive") {
-        alert("Document is interactive");
-    } else if (document.readyState === "complete") {
-        alert("Document is fully loaded");
-    }
-};
+//In general, it is recommended to use the defer attribute for scripts that need to access the DOM,
+//and the async attribute for scripts that do not depend on the DOM or other scripts.
